11193ed9e8e2e345b24dd3243c8e39e57ecb5b0d
Wikum <wikumdina@gmail.com>
2020-09-30 19:32:09 -0400

    Adding main functions

---
+++ R/main.R
@@ -0,0 +1,25 @@
+#' Cover a proportion of a given binary set with the smallest number of features 
+#'
+#' Function for computing the minimal covering for a given binary data matrix given 
+#' a minimum proportion of samples to cover
+#'
+#' @param mat A binary data matrix with each column corresponding to a sample and each row 
+#' corresponding to a feature.
+#'
+#' @return A list.
+#'
+#' @export
+#'
+#' @examples
+#'
+
+computeCovering <- function(mat){
+	getCovering(mat)
+}
+
+# ==================================================================================================
+
+
+
+
+
