9400bbd3efe98a8170bc0dc1944dc6ee2d026214
Wikum <wikumdina@gmail.com>
2020-10-21 21:02:27 -0400

    Updating help

--- R/main.R
+++ R/main.R
@@ -6,7 +6,7 @@
 #' @param mat A binary data matrix with each column corresponding to a sample and each row 
 #' corresponding to a feature.
 #'
-#' @param alpha A value in the [0, 1) range indicating what proportion of samples to be 
+#' @param alpha A value in the 0 <= alpha < 1 range indicating what proportion of samples to be 
 #' considered as outlier. By default alpha = 0.05, indicating 95% of samples to be covered.
 #' 
 #' @param maxsol The number of optimal solutions to be returned. Default is 100.
